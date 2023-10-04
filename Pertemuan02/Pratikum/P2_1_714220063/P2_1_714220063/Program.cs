bf154c45475c4e4ddbbcdb4cf5b1b118f36c0ddb
nekowawolf <irukainu20@gmail.com>
2023-10-04 16:18:10 +0700

    first commit

---
+++ Pertemuan02/Pratikum/P2_1_714220063/P2_1_714220063/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714220063
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan Angka Pertama : ");
+            float angkaPertama = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukan Angka Kedua : ");
+            float angkaKedua = Convert.ToInt32(Console.ReadLine());
+
+            float hasilPenjumblahan = angkaPertama + angkaKedua; Console.WriteLine($"Penjumblahan:{angkaPertama} + {angkaKedua} = {hasilPenjumblahan}");
+
+            float hasilPengurangan = angkaPertama - angkaKedua; Console.WriteLine($"Pengurangan:{angkaPertama} - {angkaKedua} = {hasilPengurangan}");
+
+            float hasilPerkalian = angkaPertama * angkaKedua; Console.WriteLine($"Perkalian:{angkaPertama} * {angkaKedua} = {hasilPerkalian}");
+
+            float hasilPembagian = angkaPertama / angkaKedua; Console.WriteLine($"Perkalian:{angkaPertama} / {angkaKedua} = {hasilPembagian}");
+
+        }
+    }
+}
