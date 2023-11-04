7545f9aef072edb01f64656c6795f492319ccf42
nekowawolf <irukainu20@gmail.com>
2023-11-05 03:36:51 +0700

    uniswap

---
+++ Pertemuan06/Praktikum/P6_4_714220063/P6_4_714220063/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace P6_4_714220063
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tb_umur = new System.Windows.Forms.TextBox();
+            this.cb_gender = new System.Windows.Forms.ComboBox();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ETHEREUM L2 DEV REGISTRATION FORM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(78, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(78, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(176, 100);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(122, 20);
+            this.tbNama.TabIndex = 5;
+            this.tbNama.Leave += new System.EventHandler(this.tbNama_Leave);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(176, 175);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(122, 20);
+            this.tbEmail.TabIndex = 6;
+            this.tbEmail.Leave += new System.EventHandler(this.tbEmail_Leave);
+            // 
+            // tb_umur
+            // 
+            this.tb_umur.Location = new System.Drawing.Point(176, 247);
+            this.tb_umur.Name = "tb_umur";
+            this.tb_umur.Size = new System.Drawing.Size(42, 20);
+            this.tb_umur.TabIndex = 7;
+            this.tb_umur.Leave += new System.EventHandler(this.tb_umur_Leave);
+            // 
+            // cb_gender
+            // 
+            this.cb_gender.BackColor = System.Drawing.SystemColors.Window;
+            this.cb_gender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_gender.FormattingEnabled = true;
+            this.cb_gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cb_gender.Location = new System.Drawing.Point(176, 314);
+            this.cb_gender.Name = "cb_gender";
+            this.cb_gender.Size = new System.Drawing.Size(121, 21);
+            this.cb_gender.TabIndex = 8;
+            this.cb_gender.Leave += new System.EventHandler(this.cb_gender_Leave);
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(176, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "SUBMIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(261, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(176, 375);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Upload CV";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 482);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cb_gender);
+            this.Controls.Add(this.tb_umur);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ethereum";
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tb_umur;
+        private System.Windows.Forms.ComboBox cb_gender;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Pertemuan06/Praktikum/P6_4_714220063/P6_4_714220063/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_4_714220063
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbNama_Leave(object sender, EventArgs e)
+        {
+            if (tbNama.Text == "")
+            {
+                epWarning.SetError(tbNama, "");
+                epWrong.SetError(tbNama, "Can't be Empty");
+                epCorrect.SetError(tbNama, "");
+            }
+            else if ((tbNama.Text).All(Char.IsUpper))
+            {
+                epWarning.SetError(tbNama, "");
+                epWrong.SetError(tbNama, "");
+                epCorrect.SetError(tbNama, "Correct");
+            }
+            else
+            {
+                epWarning.SetError(tbNama, "Inputs can only be uppercase!");
+                epWrong.SetError(tbNama, "");
+                epCorrect.SetError(tbNama, "");
+            }
+        }
+
+        private void tbEmail_Leave(object sender, EventArgs e)
+        {
+            if (tbEmail.Text == "")
+            {
+                epWarning.SetError(tbEmail, "");
+                epWrong.SetError(tbEmail, "Can't be Empty");
+                epCorrect.SetError(tbEmail, "");
+            }
+            else if (tbEmail.Text.All(c => Char.IsLower(c) || Char.IsDigit(c) || c == '@' || c == '.') && Regex.IsMatch(tbEmail.Text, @"^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epWarning.SetError(tbEmail, "");
+                epWrong.SetError(tbEmail, "");
+                epCorrect.SetError(tbEmail, "Corrcet");
+            }
+            else
+            {
+                epWarning.SetError(tbEmail, "Wrong email format must be lowercase!\nExample: a@b.c");
+                epWrong.SetError(tbEmail, "");
+                epCorrect.SetError(tbEmail, "");
+            }
+        }
+
+        private void tb_umur_Leave(object sender, EventArgs e)
+        {
+            if (tb_umur.Text == "")
+            {
+                epWarning.SetError(tb_umur, "");
+                epWrong.SetError(tb_umur, "Can't be Empty");
+                epCorrect.SetError(tb_umur, "");
+            }
+            else if (tb_umur.Text.Length != 2 || !tb_umur.Text.All(Char.IsDigit))
+            {
+                epWarning.SetError(tb_umur, "Input only 2-digit numbers");
+                epWrong.SetError(tb_umur, "");
+                epCorrect.SetError(tb_umur, "");
+            }
+            else
+            {
+                epWarning.SetError(tb_umur, "");
+                epWrong.SetError(tb_umur, "");
+                epCorrect.SetError(tb_umur, "Corrcet");
+            }
+        }
+
+        private void cb_gender_Leave(object sender, EventArgs e)
+        {
+            if (cb_gender.Text == "")
+            {
+                epWarning.SetError(cb_gender, "");
+                epWrong.SetError(cb_gender, "Can't be Empty");
+                epCorrect.SetError(cb_gender, "");
+            }
+            else if (cb_gender.SelectedIndex != -1)
+            {
+                epWarning.SetError(cb_gender, "");
+                epWrong.SetError(cb_gender, "");
+                epCorrect.SetError(cb_gender, "Corrcet");
+            }
+            else
+            {
+                epWarning.SetError(cb_gender, "Choose your gender!");
+                epWrong.SetError(cb_gender, "");
+                epCorrect.SetError(cb_gender, "");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbNama.Text) || !tbNama.Text.All(Char.IsUpper))
+            {
+                MessageBox.Show("The name must be filled in and can only be capitalized.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cb_gender.Text) || cb_gender.SelectedIndex == -1)
+            {
+                MessageBox.Show("Gender must be selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(tbEmail.Text) || !Regex.IsMatch(tbEmail.Text, @"^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                MessageBox.Show("Emails must be filled in the correct format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(tb_umur.Text) || tb_umur.Text.Length != 2 || !tb_umur.Text.All(Char.IsDigit))
+            {
+                MessageBox.Show("Age must be filled in with a 2-digit number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Filter = "PDF Files|*.pdf|Word Documents|*.doc;*.docx|All Files|*.*";
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                MessageBox.Show("Please select a CV file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Filter = "All Files|*.*"; 
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                string selectedFilePath = openFileDialog.FileName;
+                
+            }
+        }
+    }
+}
