67d8d55152e92497a2d7a1936d7f06074a25326a
nekowawolf <irukainu20@gmail.com>
2023-11-10 01:25:05 +0700

    just help me out

---
+++ Pertemuan07/Praktikum/P7_1_714220063/P7_1_714220063/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace P7_1_714220063
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbProdi = new System.Windows.Forms.TextBox();
+            this.tbKelas = new System.Windows.Forms.TextBox();
+            this.btnCek = new System.Windows.Forms.Button();
+            this.btnTutupForm = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbSenin = new System.Windows.Forms.RadioButton();
+            this.rbMinggu = new System.Windows.Forms.RadioButton();
+            this.cbKuliah = new System.Windows.Forms.CheckBox();
+            this.cbTravelling = new System.Windows.Forms.CheckBox();
+            this.cbTidur = new System.Windows.Forms.CheckBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnResetForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prodi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelas";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(123, 30);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(168, 20);
+            this.tbNama.TabIndex = 3;
+            // 
+            // tbProdi
+            // 
+            this.tbProdi.Location = new System.Drawing.Point(123, 93);
+            this.tbProdi.Name = "tbProdi";
+            this.tbProdi.Size = new System.Drawing.Size(100, 20);
+            this.tbProdi.TabIndex = 4;
+            // 
+            // tbKelas
+            // 
+            this.tbKelas.Location = new System.Drawing.Point(123, 149);
+            this.tbKelas.Name = "tbKelas";
+            this.tbKelas.Size = new System.Drawing.Size(100, 20);
+            this.tbKelas.TabIndex = 5;
+            // 
+            // btnCek
+            // 
+            this.btnCek.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCek.Location = new System.Drawing.Point(123, 213);
+            this.btnCek.Name = "btnCek";
+            this.btnCek.Size = new System.Drawing.Size(75, 23);
+            this.btnCek.TabIndex = 6;
+            this.btnCek.Text = "Check";
+            this.btnCek.UseVisualStyleBackColor = true;
+            this.btnCek.Click += new System.EventHandler(this.btnCek_Click);
+            // 
+            // btnTutupForm
+            // 
+            this.btnTutupForm.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTutupForm.Location = new System.Drawing.Point(231, 213);
+            this.btnTutupForm.Name = "btnTutupForm";
+            this.btnTutupForm.Size = new System.Drawing.Size(75, 23);
+            this.btnTutupForm.TabIndex = 7;
+            this.btnTutupForm.Text = "Close Form";
+            this.btnTutupForm.UseVisualStyleBackColor = true;
+            this.btnTutupForm.Click += new System.EventHandler(this.btnTutupForm_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 35);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "LIST KEGIATAN";
+            // 
+            // rbSenin
+            // 
+            this.rbSenin.AutoSize = true;
+            this.rbSenin.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSenin.Location = new System.Drawing.Point(36, 362);
+            this.rbSenin.Name = "rbSenin";
+            this.rbSenin.Size = new System.Drawing.Size(55, 20);
+            this.rbSenin.TabIndex = 9;
+            this.rbSenin.TabStop = true;
+            this.rbSenin.Text = "Senin";
+            this.rbSenin.UseVisualStyleBackColor = true;
+            this.rbSenin.CheckedChanged += new System.EventHandler(this.rbSenin_CheckedChanged);
+            // 
+            // rbMinggu
+            // 
+            this.rbMinggu.AutoSize = true;
+            this.rbMinggu.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMinggu.Location = new System.Drawing.Point(36, 398);
+            this.rbMinggu.Name = "rbMinggu";
+            this.rbMinggu.Size = new System.Drawing.Size(62, 20);
+            this.rbMinggu.TabIndex = 10;
+            this.rbMinggu.TabStop = true;
+            this.rbMinggu.Text = "Minggu";
+            this.rbMinggu.UseVisualStyleBackColor = true;
+            this.rbMinggu.CheckedChanged += new System.EventHandler(this.rbMinggu_CheckedChanged);
+            // 
+            // cbKuliah
+            // 
+            this.cbKuliah.AutoSize = true;
+            this.cbKuliah.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbKuliah.Location = new System.Drawing.Point(36, 444);
+            this.cbKuliah.Name = "cbKuliah";
+            this.cbKuliah.Size = new System.Drawing.Size(59, 20);
+            this.cbKuliah.TabIndex = 11;
+            this.cbKuliah.Text = "Kuliah";
+            this.cbKuliah.UseVisualStyleBackColor = true;
+            // 
+            // cbTravelling
+            // 
+            this.cbTravelling.AutoSize = true;
+            this.cbTravelling.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTravelling.Location = new System.Drawing.Point(36, 480);
+            this.cbTravelling.Name = "cbTravelling";
+            this.cbTravelling.Size = new System.Drawing.Size(75, 20);
+            this.cbTravelling.TabIndex = 12;
+            this.cbTravelling.Text = "Traveling";
+            this.cbTravelling.UseVisualStyleBackColor = true;
+            // 
+            // cbTidur
+            // 
+            this.cbTidur.AutoSize = true;
+            this.cbTidur.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTidur.Location = new System.Drawing.Point(36, 515);
+            this.cbTidur.Name = "cbTidur";
+            this.cbTidur.Size = new System.Drawing.Size(55, 20);
+            this.cbTidur.TabIndex = 13;
+            this.cbTidur.Text = "Tidur";
+            this.cbTidur.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(123, 550);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 14;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnResetForm
+            // 
+            this.btnResetForm.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResetForm.Location = new System.Drawing.Point(231, 550);
+            this.btnResetForm.Name = "btnResetForm";
+            this.btnResetForm.Size = new System.Drawing.Size(75, 23);
+            this.btnResetForm.TabIndex = 15;
+            this.btnResetForm.Text = "Resrt Form";
+            this.btnResetForm.UseVisualStyleBackColor = true;
+            this.btnResetForm.Click += new System.EventHandler(this.btnResetForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 598);
+            this.Controls.Add(this.btnResetForm);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.cbTidur);
+            this.Controls.Add(this.cbTravelling);
+            this.Controls.Add(this.cbKuliah);
+            this.Controls.Add(this.rbMinggu);
+            this.Controls.Add(this.rbSenin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTutupForm);
+            this.Controls.Add(this.btnCek);
+            this.Controls.Add(this.tbKelas);
+            this.Controls.Add(this.tbProdi);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form malas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbProdi;
+        private System.Windows.Forms.TextBox tbKelas;
+        private System.Windows.Forms.Button btnCek;
+        private System.Windows.Forms.Button btnTutupForm;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbSenin;
+        private System.Windows.Forms.RadioButton rbMinggu;
+        private System.Windows.Forms.CheckBox cbKuliah;
+        private System.Windows.Forms.CheckBox cbTravelling;
+        private System.Windows.Forms.CheckBox cbTidur;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnResetForm;
+    }
+}
+
---
+++ Pertemuan07/Praktikum/P7_1_714220063/P7_1_714220063/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P7_1_714220063
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(356, 296);
+        }
+
+        private void btnCek_Click(object sender, EventArgs e)
+        {
+            string errorMessage = "";
+
+            if (string.IsNullOrWhiteSpace(tbNama.Text))
+            {
+                errorMessage += "Nama belum diisi\n";
+            }
+            if (string.IsNullOrWhiteSpace(tbProdi.Text))
+            {
+                errorMessage += "Prodi belum diisi\n";
+            }
+            else if (!Regex.IsMatch(tbProdi.Text, @"^[A-Za-z0-9]+-[A-Za-z0-9]+$"))
+            {
+                errorMessage += "Prodi harus berformat [Strata]-[Prodi]\n";
+            }
+
+            if (string.IsNullOrWhiteSpace(tbKelas.Text))
+            {
+                errorMessage += "Kelas belum diisi\n";
+            }
+
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                MessageBox.Show("Lengkap", "Informasi Data Submit", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Size = new Size(356, 637);
+            }
+            else
+            {
+                MessageBox.Show(errorMessage, "Informasi Data Submit", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnTutupForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rbSenin_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbSenin.Checked)
+            {
+                cbKuliah.Enabled = true; cbKuliah.Checked = true;
+                cbTidur.Enabled = true; cbTidur.Checked = true;
+                cbTravelling.Enabled = false; cbTravelling.Checked = false;
+            }
+        }
+
+        private void rbMinggu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbMinggu.Checked)
+            {
+                cbKuliah.Enabled = false; cbKuliah.Checked = false;
+                cbTidur.Enabled = true; cbTidur.Checked = true;
+                cbTravelling.Enabled = true; cbTravelling.Checked = true;
+            }
+        }
+
+        private void btnResetForm_Click(object sender, EventArgs e)
+        {
+            //tbNama.Text = null;
+            //tbKelas.Text = null;
+            //tbProdi.Text = null;
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox)
+                    ((TextBox)control).Text = string.Empty;
+                else if (control is RadioButton)
+                    ((RadioButton)control).Checked = false;
+                else if (control is CheckBox)
+                    ((CheckBox)control).Checked = false;
+                //else if (control is ComboBox)
+                //((ComboBox)control).SelectedIndex = -1;
+            }
+            this.Size = new Size(356, 296);
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            string pilihhari = haridipilih();
+            string haridipilih()
+            {
+                if (rbMinggu.Checked)
+                {
+                    return rbMinggu.Text;
+                }
+                else if (rbSenin.Checked)
+                {
+                    return rbSenin.Text;
+                }
+                return "";
+            }
+
+            string pilihkegiatan = kegiatandipilih();
+            string kegiatandipilih()
+            {
+                List<string> selectedActivitiesList = new List<string>();
+
+                if (cbTidur.Checked)
+                {
+                    selectedActivitiesList.Add(cbTidur.Text);
+                }
+                if (cbKuliah.Checked)
+                {
+                    selectedActivitiesList.Add(cbKuliah.Text);
+                }
+                if (cbTravelling.Checked)
+                {
+                    selectedActivitiesList.Add(cbTravelling.Text);
+                }
+                return string.Join(", ", selectedActivitiesList);
+            }
+
+            string errorMsg = "";
+
+            if (string.IsNullOrWhiteSpace(tbNama.Text))
+            {
+                errorMsg += "Nama belum diisi\n";
+            }
+            if (string.IsNullOrWhiteSpace(tbProdi.Text))
+            {
+                errorMsg += "Prodi belum diisi\n";
+            }
+            else if (!Regex.IsMatch(tbProdi.Text, @"^[A-Za-z0-9]+-[A-Za-z0-9]+$"))
+            {
+                errorMsg += "Prodi harus berformat [Strata]-[Prodi]\n";
+            }
+
+            if (string.IsNullOrWhiteSpace(tbKelas.Text))
+            {
+                errorMsg += "Kelas belum diisi\n";
+            }
+
+            if (string.IsNullOrEmpty(pilihhari))
+            {
+                errorMsg += "Pilih salah satu hari.\n";
+            }
+
+            if (string.IsNullOrEmpty(pilihkegiatan))
+            {
+                errorMsg += "Pilih setidaknya satu kegiatan.\n";
+            }
+
+            if (string.IsNullOrEmpty(errorMsg))
+            {
+                MessageBox.Show($"Nama : {tbNama.Text}\nProdi : {tbProdi.Text}\nKelas : {tbKelas.Text}\nHari : {pilihhari}\nKegiatan : {pilihkegiatan}", "Cetak Berhasil!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(errorMsg, "Informasi Data Submit", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    } 
+}
